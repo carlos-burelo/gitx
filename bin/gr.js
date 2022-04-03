ca6c56570ad10750432338b46e4de491e897f51a
carlos-burelo <carlosfernandoburelo@gmail.com>
2022-04-03 16:47:41 -0500

    :arrow_up: update git remote command

--- bin/gr.js
+++ bin/gr.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 'use strict'
 
-import { ctx as url, run } from './index.js'
+import { ctx as data, run } from './index.js'
 ;(() => {
+  const match = /^https:\/\/github\.com\/([^\/]+)\/([^\/]+)\/?$/.exec(data)
+  const url = match ? data : `https://github.com/${data}`
   const cmd = `git remote add origin ${url || '.'}`
   run(cmd)
 })()
