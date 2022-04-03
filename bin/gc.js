80e93b598c59ba59ec303e4d85693e1a0d5ac161
carlos-burelo <carlosfernandoburelo@gmail.com>
2022-04-03 16:57:34 -0500

    :bug: update some info in git commit

--- bin/gc.js
+++ bin/gc.js
@@ -4,5 +4,9 @@
 import { ctx as data, run } from './index.js'
 ;(() => {
   const cmd = `git commit -m "${data || ':jigsaw: base commit'}"`
-  run(cmd)
+  run(cmd, handler)
 })()
+
+function handler(stdout) {
+  return stdout.replace(/\[.+\].+/, '').trim()
+}
